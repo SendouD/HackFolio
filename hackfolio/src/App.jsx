2ac454830c355d34c189cf3f7cfe83456dbacbef
Laxical <vinnu24.vinay@gmail.com>
2024-09-19 14:42:24 +0530

    minor changes

--- hackfolio/src/App.jsx
+++ hackfolio/src/App.jsx
@@ -10,11 +10,10 @@ import Project_display from "./pages/ProjectDisplay";
 import UserProjects from "./pages/UserProjects";
 import HackathonProjectDispay from "./pages/hackathon_projects";
 import Hackathon_page from "./pages/Hackathon_page";
-import Org_form3 from "./components/Org_form3";
 import Hack_cards_disp from "./pages/Hack_cards_disp"
 
 function App() {
-  
+
   
   return(
     <>
