3fd664065ffaf50c680e179edd20a29ce1c2e382
jananathan <113715330+SendouD@users.noreply.github.com>
2024-09-22 13:51:48 +0530

    admin dashboard

--- hackfolio/src/App.jsx
+++ hackfolio/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import HomePage from "./pages/HomePage";
 import AuthCheck from "./components/AuthCheck";
+import AdminDashboard from "./pages/AdminDashboard";
 
 
 //Projects
@@ -30,15 +31,27 @@ function App() {
     <>
       <Routes>
         <Route element={<AuthCheck/>}>
+
+
+          {/* Normal Routes */}
           <Route path="/" element={<HomePage />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route element={<Project_display/>} path="/ProjectDisplay/:projectId" />
           <Route element={<UserDashboard/>} path="/@/:username" />
+          <Route element={<SuccessPage />} path="/uploadsuccess" />
           {/* <Route path="/HackathonProjects" element={<HackathonProjectDispay/>}/> */}
           <Route path="/projectForm" element={<ProjectSubmissionForm />}/>
-          {/* <Route path="/uploadsuccess" element={<SuccessPage/>}/> */}
+          <Route element={<SuccessPage />} path="/uploadsuccess" />
+          <Route path="/hackathons" element={<AllHackathonsDisplay />} />
+          <Route path="/hackathon/:name" element={<HackathonWebpage />} />
+          <Route path="/organizedHackathons/:name" element={<EditOrganizedHackathonDetails />} /> 
 
+
+
+
+
+          {/* User private routes */}
           <Route element={<PrivateRoute role="User" />}>
             <Route path="/createHackathon" element={<CreateHackathon />} />
             <Route path="/completeHackathonCreation/:name" element={<FillFullHackathonDetails />} />
@@ -46,14 +59,18 @@ function App() {
             <Route path="/hackathon/:name/register" element={<HackathonRegistrationPage />} />
             <Route element={<UserProjects />} path="/userProjects" />
           </Route>
+
+
+
+
+          {/* Admin Private Routes */}
           <Route element={<PrivateRoute role="Admin" />}>
+          <Route element={<AdminDashboard />} path="/adminDashboard" />
        
-            <Route element={<SuccessPage />} path="/uploadsuccess" />
+            
          
           </Route>
-          <Route path="/hackathons" element={<AllHackathonsDisplay />} />
-          <Route path="/hackathon/:name" element={<HackathonWebpage />} />
-          <Route path="/organizedHackathons/:name" element={<EditOrganizedHackathonDetails />} />        
+              
         </Route>
 
       </Routes>
