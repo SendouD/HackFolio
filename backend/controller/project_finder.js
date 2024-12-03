8f76ef2bafaae6985f53742f920ca5936bfcf13a
jananathan <janan.karur@gmail.com>
2024-12-04 00:31:37 +0530

    pull req

--- backend/controller/project_finder.js
+++ backend/controller/project_finder.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const Project = require("../models/projectForm_Schema");
 const teamCodeSchema = require('../models/team_code_schema');
-const teamCodeSchema = require('../models/team_code_schema');
 const hackParticipantDetails = require('../models/hackathon_participants_schema');
 const isUser = require("../middleware/isUser");
 
@@ -102,11 +101,6 @@ router
     try {
       let projectId = req.params.id;
 
-router
-  .get("/:id", async (req, res) => {
-    try {
-      let projectId = req.params.id;
-
       const projects = await Project.findOne({ _id: projectId });
       if (projects.length === 0) {
         return res.status(404).json({
@@ -164,54 +158,54 @@ router
   });
 
 
-      res.status(200).json(projects);
-    } catch (error) {
-      // Handle errors during retrieval
-      console.error("Error fetching project details:", error);
-      res.status(500).json({
-        message: "Error fetching project details",
-        error: error.message,
-      });
-    }
-  })
-  .put("/:id", isUser, async (req, res) => {
-    const { id } = req.params;
-    const data = req.body;
-    const username = req.username;
-    const email = req.email;
-    let flag = 0;
+  //     res.status(200).json(projects);
+  //   } catch (error) {
+  //     // Handle errors during retrieval
+  //     console.error("Error fetching project details:", error);
+  //     res.status(500).json({
+  //       message: "Error fetching project details",
+  //       error: error.message,
+  //     });
+  //   }
+  // })
+  // .put("/:id", isUser, async (req, res) => {
+  //   const { id } = req.params;
+  //   const data = req.body;
+  //   const username = req.username;
+  //   const email = req.email;
+  //   let flag = 0;
 
-    try {
-      const projDetails = await Project.findOne({_id: id});
-      if(projDetails.username === username) flag = 1;
-      if(projDetails.teamCode !== '') {
-        const team = await teamCodeSchema.findOne({teamCode: projDetails.teamCode, hackathonName: projDetails.hackathonName});
-        for(let i=0;i<team.members.length;i++) {
-          if(team.members[i] === email) {
-            flag = 1;
-            break;
-          }
-        }
-      }
-      console.log(flag);
-      if(flag === 0) {
-        res.status(400).json({ Error: "User not authorized to Edit project!" });
-      }
+  //   try {
+  //     const projDetails = await Project.findOne({_id: id});
+  //     if(projDetails.username === username) flag = 1;
+  //     if(projDetails.teamCode !== '') {
+  //       const team = await teamCodeSchema.findOne({teamCode: projDetails.teamCode, hackathonName: projDetails.hackathonName});
+  //       for(let i=0;i<team.members.length;i++) {
+  //         if(team.members[i] === email) {
+  //           flag = 1;
+  //           break;
+  //         }
+  //       }
+  //     }
+  //     console.log(flag);
+  //     if(flag === 0) {
+  //       res.status(400).json({ Error: "User not authorized to Edit project!" });
+  //     }
 
-      const updatedProject = await Project.findByIdAndUpdate(
-          id,
-          { ...data },
-      );
+  //     const updatedProject = await Project.findByIdAndUpdate(
+  //         id,
+  //         { ...data },
+  //     );
 
-      if (!updatedProject) {
-          return res.status(404).json({ message: "Project not found" });
-      }
+  //     if (!updatedProject) {
+  //         return res.status(404).json({ message: "Project not found" });
+  //     }
 
-      res.status(200).json(updatedProject);
-    } catch (e) {
-      res.status(400).json({ Error: e.message });
-    }
-  });
+  //     res.status(200).json(updatedProject);
+  //   } catch (e) {
+  //     res.status(400).json({ Error: e.message });
+  //   }
+  // });
 
 
 router.get("/hackathonprojects/:id", isUser, async (req, res) => {
