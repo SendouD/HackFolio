7a1cc9213ee749900da389186ca59bfa24a43a01
Ashitaka <vinnu24.vinay@gmail.com>
2025-05-04 19:07:03 +0530

    bug fix

--- backend/middleware/redisClient.js
+++ backend/middleware/redisClient.js
@@ -1,20 +1,12 @@
+// redisClient.js
 const redis = require('redis');
 
-const redisUrl = process.env.REDIS_URL;
-
-const redisClient = redis.createClient({
-  url: redisUrl,
-});
+const redisClient = redis.createClient();
 
 redisClient.on('error', (err) => console.error('Redis Client Error:', err));
 
 (async () => {
-  try {
-    await redisClient.connect();
-    console.log('Connected to Redis');
-  } catch (err) {
-    console.error('Failed to connect to Redis:', err);
-  }
+  await redisClient.connect();
 })();
 
 module.exports = redisClient;
