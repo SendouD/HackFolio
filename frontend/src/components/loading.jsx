1e755a5853e47612f2f4d77a980f574b1506d27e
SendouD <janan.karur@gmail.com>
2025-05-05 01:13:54 +0530

    nav bar changes

--- frontend/src/components/loading.jsx
+++ frontend/src/components/loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen ">
       <div className="text-center">
         <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-[#5f3abd]"></div>
         <p className="mt-4 text-lg text-gray-600">Loading...</p>
