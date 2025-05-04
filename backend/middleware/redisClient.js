61005a34e587b1514f5d5dedf16339da9ebfd349
Ashitaka <vinnu24.vinay@gmail.com>
2025-05-04 18:51:33 +0530

    redis url change for production

--- backend/middleware/redisClient.js
+++ backend/middleware/redisClient.js
@@ -1,12 +1,20 @@
-// redisClient.js
 const redis = require('redis');
 
-const redisClient = redis.createClient();
+const redisUrl = process.env.REDIS_URL;
+
+const redisClient = redis.createClient({
+  url: redisUrl,
+});
 
 redisClient.on('error', (err) => console.error('Redis Client Error:', err));
 
 (async () => {
-  await redisClient.connect();
+  try {
+    await redisClient.connect();
+    console.log('Connected to Redis');
+  } catch (err) {
+    console.error('Failed to connect to Redis:', err);
+  }
 })();
 
 module.exports = redisClient;
